9163320b83cd4a1e8d1e0e1d3597df82fcdd3d50
stevens-bermudez <michaelgutierrez2006@gmail.com>
2024-02-07 17:29:02 -0500

    feat: card-back

--- src/App.jsx
+++ src/App.jsx
@@ -23,19 +23,19 @@ function App() {
           <section className="container__form">
             <form className="form" action="">
               <label className="form__input" htmlFor="cardholder">Cardholder Name</label>
-              <input className="form__input_" type="text" name="cardholder" maxLength="18" placeholder="e.g. Jane Appleseed" id="cardholder" />
+              <input className="form__input_" type="text" name="cardholder" maxLength="18" placeholder="e.g. Jane Appleseed" id="cardholder" pattern='[A-Za-z\s]+' autoComplete="off" />
               <div className="form__input-cardholder--error error">only character type ABC</div>
               <label className="form__input" htmlFor="cardnumber">Card Number</label>
-              <input className="form__input_" type="text" name="cardnumber" maxLength="19" placeholder="e.g. 1234 5678 9123 0000" id="cardnumber" />
+              <input className="form__input_" type="text" name="cardnumber" maxLength="19" placeholder="e.g. 1234 5678 9123 0000" id="cardnumber" pattern='[0-9\s]+' autoComplete="off" />
               <div className="form__input-cardnumber--error error">only character type Number</div>
               <div className="form__date-cvc">
                 <label className="form__input" htmlFor="">Exp. Date (MM/YY)</label>
                 <label className="form__input" htmlFor="">CVC</label>
                 <div className="form__date">
-                  <input className="form__input_" type="text" name="cardmonth" maxLength="2" placeholder="MM" id="cardmonth" />
-                  <input className="form__input_" type="text" name="cardyear" maxLength="2" placeholder="YY" id="cardyear" />
+                  <input className="form__input_" type="text" name="cardmonth" maxLength="2" placeholder="MM" id="cardmonth" pattern='[0-9\]+' />
+                  <input className="form__input_" type="text" name="cardyear" maxLength="2" placeholder="YY" id="cardyear" pattern='[0-9\]+' />
                 </div>
-                <input className="form__input_" type="text" maxLength="3" name="cardcvc" placeholder="e.g 123" id="cardcvc" />
+                <input className="form__input_" type="text" maxLength="3" name="cardcvc" placeholder="e.g 123" id="cardcvc" pattern='[A-Za-z0-9\s]+' />
                 <div className="form__error-container">
                   <div className="form__input-mm-error error">only character type date</div>
                   <div className="form__input-yy-error error">only character type date</div>
